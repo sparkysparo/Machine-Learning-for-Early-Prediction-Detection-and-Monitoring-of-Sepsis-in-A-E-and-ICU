5bf46d2c64192271614fe379d695ce42742c31f4
Paschal Uzoegwu <74314617+sparkysparo@users.noreply.github.com>
2025-03-08 12:18:25 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,7 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "provenance": [],
-      "authorship_tag": "ABX9TyOHDipQ7Mejh4FtWl+qyv00",
-      "include_colab_link": true
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/sparkysparo/Machine-Learning-for-Early-Prediction-Detection-and-Monitoring-of-Sepsis-in-A-E-and-ICU/blob/main/app.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
-    {
-      "cell_type": "code",
-      "source": [
-        "import pandas as pd\n",
-        "import numpy as np\n",
-        "import joblib\n",
-        "import time\n",
-        "import matplotlib.pyplot as plt\n",
-        "from sklearn.preprocessing import StandardScaler\n",
-        "import streamlit as st"
-      ],
-      "metadata": {
-        "id": "IQll7uAB5xwF"
-      },
-      "execution_count": null,
-      "outputs": []
-    }
-  ]
-}
+import pandas as pd
+import numpy as np
+import joblib
+import time
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+import streamlit as st
